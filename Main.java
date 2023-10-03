30c8212b6b21c135ca5340d43d73a4aef590fff2
Riyasrivastava2001 <118271265+Riyasrivastava2001@users.noreply.github.com>
2023-10-03 23:37:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers of rows and columns");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int numbers[][] = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter the element to be searched");
+        int x = sc.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (numbers[i][j] == x) {
+                    System.out.println("The position of the element is at" + "(" + i + "," + j + ")");
+                }
+            }
+
+
+        }
+    }
+}
