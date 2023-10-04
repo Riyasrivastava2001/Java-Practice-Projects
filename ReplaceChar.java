69014e3496af9c2f133e0813cfc2ef514d609468
Riyasrivastava2001 <118271265+Riyasrivastava2001@users.noreply.github.com>
2023-10-05 03:19:41 +0530

    Add files via upload

---
+++ ReplaceChar.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ReplaceChar {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the values ");
+        String str=sc.nextLine();
+
+        String result="";
+
+
+
+        for(int i=0;i<str.length();i++){
+            if (str.charAt(i)=='e'){
+                result += 'i';
+            }
+            else{
+                result+=str.charAt(i);
+            }
+        }
+        System.out.println(result);
+    }
+
+}
