a25856dfe18684f72fc0d6584450b107898052de
Riyasrivastava2001 <118271265+Riyasrivastava2001@users.noreply.github.com>
2023-10-07 23:36:58 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class  ReverseString{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder("helloworld");
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front=i;
+            int back=sb.length()-1-i;
+
+            char frontchar=sb.charAt(front);
+            char backchar=sb.charAt(back);
+
+            sb.setCharAt(front,backchar);
+            sb.setCharAt(back,frontchar);
+        }
+        System.out.println(sb);
+    }
+}
